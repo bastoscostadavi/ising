7333191c8261f241ebaf7409d515e2e0c5ed4ee5
Davi <davi.costa@usp.br>
2017-08-21 09:20:40 -0300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -11,15 +11,9 @@ temperatures = np.array(range(2,52,2))/10
 energy = []
 magnetization = []
 for T in temperatures:
-	e,m,E,M = ising(1/T,l)
-	energy += [e]
-	magnetization += [m]
-	file = open('data/energy_termalization.txt','a')
-	file.write(str(E)+'\n')
-	file.close()		
-	file = open('data/magnetization_termalization.txt','a')
-	file.write(str(M)+'\n')
-	file.close()		
+	E,M = ising(1/T,l)[2:]
+	energy += [E]
+	magnetization += [M]
 file = open('data/energy.txt','a')
 file.write(str(energy)+'\n')
 file.close()	
